6fbc9289717a0fa8eab37ed6b901a1e4a9ceb67b
Jesus Emmanuel Salcido Martinez <emmanuelem2000@hotmail.com>
2023-05-03 09:19:22 -0600

    Correcion de errores de MYSQL

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-// require package and set the port
 const express = require('express');
 const port = 3002;
-//para permitir manejo de PORT y PUT
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
 const routes = require('./routes/routes')
 const app = express();
 
-//usar Node.js body parsing middleware
-app.use(bodyParse.json());
-app.use(bodyParse.urlencoded({
-    extended: true,
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:true,
 }));
+
 routes(app);
 
-//Iniciar el servidor
 const server = app.listen(port, (error) =>{
     if (error) return console.log(`Error: ${error}`);
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,23 @@
-//Cargue la conexion del grupo MySQL
-const { request, response, Router } = require('express');
-const pool = require('../date/config');
-//Ruta de la app
-const routes = app =>{
-    //mostrar mensaje de bienvenida de root
-    app.get('/',(request, response)=>{
-        response.send({
-            message:'Biencenido a Node.js Express REST API!'
-        });
-    });
-//Mostrar todos los usuarios
-app.get('/users',(request, response)=>{
-    pool.query('SELECT * FROM users', (error, result)=>{
-        if (error) throw error;
+// Cargue la conexion del grupo MySQL
+const pool = require("../data/config");
 
-        response.send(result);
+//Ruta de la app
+const router = (app) => {
+  //Mostrar mensaje de bienvenida de root
+  app.get("/", (request, response) => {
+    response.send({
+      message: "Bienvenido a Node.js Express REST API!",
     });
-});
+  });
+
+  //Mostrar todos los usuarios
+  app.get("/users", (request, response) => {
+    pool.query("SELECT * FROM users", (error, result) => {
+      if (error) throw error;
+
+      response.send(result);
+    });
+  });
 };
-module.exports = router;
+//Explorar el router
+module.exports = router;
